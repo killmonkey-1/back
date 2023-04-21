564991dfb43d2a0910f5811a43e1eab13390ed18
Killmonkey <kirya.polovinkin.00@mail.ru>
2023-04-21 19:48:47 +0500

    12

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,114 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+    <div style="display: flex;">
+      
+        <button @click="folderList" class="plus">+</button>
+        <div class="pages">
+          Добавить папку
+        </div>
+      
+        
+        <div v-if="folderList === 0"
+        >
+        добавленых постов нет
+        </div>
+      <div class="pages">
+        Добавить папку
+      </div>
 
+    </div>  
+  </div>
+
+
+   
+
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name:'App',
+    data(){
+        return{
+        folder:{
+        folderName:'',
+        folderColor:'',
+        folderTasks:[],
+            },
+        }
+    },
+    methods:{
+      folderList(){
+         this.$emit
+      },
+    }
+
 }
+
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  background-color:  #F4F6F8;;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 200px;;
+  height: 1000vh;
+  border-right: 3px solid #F1F1F1;
 }
+body{
+  position: relative;
+width: 746px;
+height: 529px;
+
+background:  #FFFFFF;
+  margin: 0%;
+  padding: 0%;
+}
+
+.line{
+  position: absolute;
+width: 1px;
+height: 700px;
+left: 200px;
+top: 0px;
+
+
+background: #F1F1F1;
+transform: matrix(-1, 0, 0, 1, 0, 0);
+}
+.plus{
+color: #868686;
+font-size: 30px;
+position: absolute;
+height: 35px;
+width: 30px;
+left: 32px;
+top: 333px;
+border: none;
+background-color: #F4F6F8;
+cursor: pointer;
+}
+.pages{
+  position: absolute;
+width: 107px;
+height: 17px;
+left: 52px;
+top: 323px;
+margin-top: 20px;
+margin-left: 10px;
+
+
+font-family: 'Lato';
+font-style: normal;
+font-weight: 600;
+font-size: 14px;
+line-height: 17px;
+/* identical to box height */
+
+letter-spacing: 0.15px;
+
+color: #767676;
+
+}
+
 </style>
